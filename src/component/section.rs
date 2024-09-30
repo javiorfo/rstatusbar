849c2dcbcb1590f147daf8b465a34d6fd34248e2
Javier Orfo <javierorfo@prontonmail.com>
2024-09-30 19:31:58 -0300

    icon name spaces

--- src/component/section.rs
+++ src/component/section.rs
@@ -8,10 +8,23 @@ pub struct Component<'a> {
 
 impl Display for Component<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let icon_str = get_final_string(self.icon);
+        let name_str = get_final_string(self.name);
+
         if self.value.is_empty() {
-            write!(f, " {} {} ", self.icon, self.name)
+            write!(f, " {}{}", icon_str, name_str)
         } else {
-            write!(f, " {} {} {} ", self.icon, self.name, self.value)
+            write!(f, " {}{}{} ", icon_str, name_str, self.value)
         }
+    }
+}
+
+fn get_final_string(string: &str) -> String {
+    if string.is_empty() {
+        String::from("")
+    } else {
+        let mut with_trailing_space = string.to_string();
+        with_trailing_space.push(' ');
+        with_trailing_space
     }
 }
